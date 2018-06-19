5840df01030d227fb0d6512ac87b8c1db4ec9480
yugecin <yugecin@users.noreply.github.com>
2018-06-20 00:01:29 +0200

    put all output files in out/ folder

--- compile.bat
+++ compile.bat
@@ -4,28 +4,29 @@ SET "_MAKE=K:\cygwin64\bin\make.exe"
 SET "_SED=K:\Program Files\Git\usr\bin\sed.exe"
 SET "_PAWNCC=..\..\..\pawno\pawncc-original.exe"
 
+IF EXIST out RD out /S /Q
+
 IF [%~1] EQU [clean] (
 	::"%_MAKE%" clean
 	IF EXIST p RD p /S /Q
-	IF EXIST basdon.amx DEL basdon.amx /Q
-	IF EXIST basdon.lst DEL basdon.lst /Q
-	IF EXIST basdon.asm DEL basdon.asm /Q
-	IF EXIST basdon.xml DEL basdon.xml /Q
 	EXIT
 )
 
+MKDIR out
 IF NOT EXIST p MKDIR p
 
-"%_MAKE%" basdon
-IF %ERRORLEVEL% NEQ 0 EXIT
-ECHO.
-"%_PAWNCC%" -(- -;- -ivendor/ -Dp/ %~1 basdon.p -r
-IF %ERRORLEVEL% NEQ 0 EXIT
-ECHO.
-IF EXIST "p\basdon.xml" (
-	MOVE "p\basdon.xml" basdon.xml>NUL
-	"%_SED%" -i 's/xml-stylesheet href="[^"]*"/xml-stylesheet href="pawndoc.xsl"/' basdon.xml
+IF [%~1] EQU [make] (
+	"%_MAKE%" %~2
+	EXIT
 )
-IF EXIST "p\basdon.lst" MOVE "p\basdon.lst" basdon.lst>NUL
-IF EXIST "p\basdon.asm" MOVE "p\basdon.asm" basdon.asm>NUL
-IF EXIST "p\basdon.amx" MOVE "p\basdon.amx" basdon.amx>NUL
+
+"%_MAKE%" build
+IF %ERRORLEVEL% NEQ 0 EXIT
+ECHO.
+"%_PAWNCC%" -(- -;- -ivendor/ -Dp/ %~1 basdon.p -r../out/basdon.xml -o../out/basdon.amx
+IF %ERRORLEVEL% NEQ 0 EXIT
+ECHO.
+IF EXIST "out\basdon.xml" (
+	"%_SED%" -i 's/xml-stylesheet href="[^"]*"/xml-stylesheet href="pawndoc.xsl"/' out/basdon.xml
+)
+
