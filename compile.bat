c341e2b84d195c9d015522150d103e7ac52d8c93
yugecin <yugecin@users.noreply.github.com>
2021-01-03 17:52:59 +0100

    Cleanup files
    Vendor files haven't been used in a while, simplify compile

--- compile.bat
+++ compile.bat
@@ -1,15 +1,4 @@
-@ECHO OFF
-
 IF NOT EXIST out MKDIR out
 DEL /Q out
-
-IF [%~1] EQU [clean] GOTO:EOF
-
-SET "_PAWNCC=pawncc.exe"
-"%_PAWNCC%">NUL 2>NUL
-IF ERRORLEVEL 9009 (
-	SET "_PAWNCC=%CD%\..\..\..\pawno\pawncc-original.exe"
-)
-"%_PAWNCC%" -(- -;- -i"%CD%/vendor/" -p -d0 -O1 -v2 %* -oout/basdon.amx basdon.pwn
+pawncc -(- -;- -p -d0 -O1 -v2 -oout/basdon.amx basdon.pwn
 IF EXIST basdon.xml DEL basdon.xml
-
--- readme.txt
+++ readme.txt
@@ -1,7 +1,4 @@
 The Basdon.net aviation server gamemode script.  The script is deprecated and
-will eventually be almost completely replaced with fly-plugin.
+will eventually be completely replaced with fly-plugin.
 
 This project is licensed under the GNU AGPLv3 License.
-Files in vendor/ do NOT fall under this License.  Check each file's heading to
-see who they belong to.
-
